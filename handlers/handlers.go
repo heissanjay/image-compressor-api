793d5c46c4f7c7f8c71ef444f8837b79181131ce
heissanjay <heissanjay@proton.me>
2024-06-16 20:17:55 +0530

    Enhancements

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -19,30 +19,31 @@ func HandlePing(c *gin.Context) {
 }
 
 func HandleCompress(c *gin.Context) {
-	contentType := c.GetHeader("Content-Type")
-	if contentType != "image/jpeg" && contentType != "image/png" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Unsupported file type"})
-		return
-	}
 
-	data, err := io.ReadAll(c.Request.Body)
+	uploadedFile, err := c.FormFile("file")
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to read request body"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "error processing file"})
 		return
 	}
 
-	img, format, err := image.Decode(bytes.NewReader(data))
+	file, _ := uploadedFile.Open()
+	defer file.Close()
+	img, format, err := image.Decode(file)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to decode image"})
 		return
 	}
 
 	var buf bytes.Buffer
-	if contentType == "image/jpeg" {
+	switch format {
+	case "jpeg":
 		quality := 75
 		err = jpeg.Encode(&buf, img, &jpeg.Options{Quality: quality})
-	} else if contentType == "image/png" {
+	case "png":
 		err = png.Encode(&buf, img)
+	default:
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Unsupported image format"})
+		return
 	}
 
 	if err != nil {
@@ -51,7 +52,6 @@ func HandleCompress(c *gin.Context) {
 	}
 
 	c.Header("Content-Disposition", "attachment; filename=compressed_image."+format)
-	c.Header("Content-Type", contentType)
 	c.Header("Content-Length", fmt.Sprint(buf.Len()))
 
 	if _, err := io.Copy(c.Writer, &buf); err != nil {
